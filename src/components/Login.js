fb65103944b323719471c01f944aff29d04f10d0
Neil Manvar <ndmanvar@yahoo.com>
2017-10-05 15:52:40 -0700

    add colon

--- src/components/Login.js
+++ src/components/Login.js
@@ -28,7 +28,7 @@ class Login extends Component {
 
                 <form onSubmit={this.handleSubmit}>
                     <div className="form-group">
-                        <label for="exampleInputEmail1">Email address</label>
+                        <label for="exampleInputEmail1">Email address:</label>
                         <input type="email" className="form-control" onChange={this.handleChange} placeholder="Enter email"/>
                         <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                     </div>
